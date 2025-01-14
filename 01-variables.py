5642735524c25ca8896106b8949350b422af0a32
IDGS-801-22001417 <82052@alumnos.utleon.edu.mx>
2025-01-13 18:55:49 -0600

    Tercer Commit

---
+++ 01-variables.py
@@ -0,0 +1,16 @@
+
+print('Ejemplo1 : ')
+
+num1=5
+num2=4
+res= num1+num2
+
+print(res)
+
+print('Dame dos numeros para sumarlos')
+print('')
+num1=int(input('Ingresa el primer numero: '))
+num2=input('Ingresa el segundo numero: ')
+suma=num1+int(num2)
+
+print('La suma de {1} mas {2} es: {0}'.format(suma,num1,num2))
